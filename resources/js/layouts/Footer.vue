eae32ff17df2a3c0af0841f26e1018492d66c5c5
haqhuy1999@gmail.com <haqhuy1999@gmail.com>
2019-11-06 09:21:22 +0700

    adasdas

--- resources/js/layouts/Footer.vue
+++ resources/js/layouts/Footer.vue
@@ -1,20 +1,16 @@
 <template>
-        <div>
-            <aside class="container">
-                    <div class="row ">
-                    <div class="col-lg-12 col-md-12-col-sm-12">
-                    footer
-                    </div>
-                    </div>
-            </aside>
-            <aside class="container">
-                    <div class="row ">
-                    <div class="col-lg-12 col-md-12-col-sm-12">
-                    footer
-                    </div>
-                    </div>
-            </aside>
-        </div>
+  <div>
+    <aside class="container">
+      <div class="row">
+        <div class="col-lg-12 col-md-12-col-sm-12">footeraaaaa</div>
+      </div>
+    </aside>
+    <aside class="container">
+      <div class="row">
+        <div class="col-lg-12 col-md-12-col-sm-12">footeraaaaa</div>
+      </div>
+    </aside>
+  </div>
 </template>
 
 <script>
